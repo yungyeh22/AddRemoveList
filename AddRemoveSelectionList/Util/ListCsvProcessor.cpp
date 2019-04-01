87d6bd6b8d5fa6117e1e608d66ffacdc9cc7d85e
Yung-Yeh Chang <yungyeh@hotmail.com>
2019-03-31 22:03:59 -0400

    Remove useless members in ListCsvProcessor class

--- AddRemoveSelectionList/Util/ListCsvProcessor.cpp
+++ AddRemoveSelectionList/Util/ListCsvProcessor.cpp
@@ -9,57 +9,25 @@ ListCsvProcessor::ListCsvProcessor() {
 
 }
 
-ListCsvProcessor::ListCsvProcessor(const QString &filename) {
-    read(filename);
-}
-
-ListCsvProcessor::ListCsvProcessor(const QStringList &sList_raw, const QStringList &sList_alias) :
-    _list_raw(sList_raw),_list_alias(sList_alias) {
-
-}
-
-int ListCsvProcessor::read(const QString &filename) {
-    if (QFile::exists(filename)) {
-        _status = readsListsFromFile(filename);
-    }
-    else {
-        _status = 1;
-    }
-    return _status;
-}
-
 int ListCsvProcessor::read(const QString &filename, QStringList &sList_raw, QStringList &sList_alias) {
     ListCsvProcessor p;
-    int status = p.read(filename);
-    p.getList(sList_raw,sList_alias);
-    return status;
-}
-
-int ListCsvProcessor::write(const QString &filename){
-    return saveListsToFile(filename, _list_raw, _list_alias);
+    return p.readsListsFromFile(filename,sList_raw,sList_alias);
 }
 
 int ListCsvProcessor::write(const QString &filename, const QStringList &sList_raw, const QStringList &sList_alias) {
-    ListCsvProcessor p(sList_raw,sList_alias);
-    return p.write(filename);
+    ListCsvProcessor p;
+    return p.saveListsToFile(filename,sList_raw,sList_alias);
 }
 
-void ListCsvProcessor::getList(QStringList &sList_raw, QStringList &sList_alias) {
-    if (_status == 0) {
-        sList_raw = _list_raw;
-        sList_alias = _list_alias;
-    }
-}
-
-int ListCsvProcessor::readsListsFromFile(const QString &filename) {
+int ListCsvProcessor::readsListsFromFile(const QString &filename, QStringList &slist_raw, QStringList &sList_alias) {
     QFile file(filename);
     if(file.open(QIODevice::ReadOnly | QIODevice::Text)) {
         QTextStream textIn(&file);
         QString lineStr;
         while (textIn.readLineInto(&lineStr)) {
             QStringList strList = lineStr.split(",");
-            _list_raw << strList.front();
-            _list_alias << strList.back();
+            slist_raw << strList.front();
+            sList_alias << strList.back();
         }
         file.close();
         _status = 0;
--- AddRemoveSelectionList/Util/ListCsvProcessor.h
+++ AddRemoveSelectionList/Util/ListCsvProcessor.h
@@ -13,21 +13,13 @@ namespace Util
 class ListCsvProcessor {
 public:
     ListCsvProcessor();
-    ListCsvProcessor(const QString &filename);
-    ListCsvProcessor(const QStringList &sList_raw, const QStringList &sList_alias);
     ~ListCsvProcessor() { ; }
-    int read(const QString &filename);
-    int static read(const QString &filename, QStringList &sList_raw, QStringList &sList_alias);
-    int write(const QString &filename);
+    int static read(const QString &filename, QStringList &sList_raw, QStringList &sList_alias);    
     int static write(const QString&filename, const QStringList &sList_raw, const QStringList &sList_alias);
-    void getList(QStringList &sListraw, QStringList &sList_alias);
-    void setList(const QStringList &sListraw, const QStringList &sList_alias);
 
 private:
-    int readsListsFromFile(const QString &filename);
-    int saveListsToFile(const QString &filename, const QStringList &sList_raw, const QStringList &sList_alias);
-    QStringList _list_raw;
-    QStringList _list_alias;
+    int readsListsFromFile(const QString &filename, QStringList &slist_raw, QStringList &sList_alias);
+    int saveListsToFile(const QString &filename, const QStringList &sList_raw, const QStringList &sList_alias);    
     int _status = 0;
 };
 
