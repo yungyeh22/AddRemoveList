473918bc64d33029fda73466e2767a797861cd7c
Yung-Yeh Chang <yungyeh@hotmail.com>
2019-04-05 10:58:27 -0400

    Re-design ListCsvProcessor class

--- AddRemoveSelectionList/Util/ListCsvProcessor.cpp
+++ AddRemoveSelectionList/Util/ListCsvProcessor.cpp
@@ -9,18 +9,52 @@ ListCsvProcessor::ListCsvProcessor() {
 
 }
 
+ListCsvProcessor::ListCsvProcessor(const QString &filename) : _filename(filename){
+
+}
+
 int ListCsvProcessor::read(const QString &filename, QStringList &sList_raw, QStringList &sList_alias) {
-    ListCsvProcessor p;
-    return p.readsListsFromFile(filename,sList_raw,sList_alias);
+    ListCsvProcessor p(filename);
+    return p.readsListsFromFile(sList_raw,sList_alias);
 }
 
 int ListCsvProcessor::write(const QString &filename, const QStringList &sList_raw, const QStringList &sList_alias) {
-    ListCsvProcessor p;
-    return p.saveListsToFile(filename,sList_raw,sList_alias);
+    ListCsvProcessor p(filename);
+    return p.saveListsToFile(sList_raw,sList_alias);
 }
 
-int ListCsvProcessor::readsListsFromFile(const QString &filename, QStringList &slist_raw, QStringList &sList_alias) {
-    QFile file(filename);
+int ListCsvProcessor::getLists(QStringList &sList_raw, QStringList &sList_alias) {
+    return readsListsFromFile(sList_raw, sList_alias);
+}
+
+int ListCsvProcessor::writeLists(const QStringList &sList_raw, const QStringList &sList_alias) {
+    return saveListsToFile(sList_raw, sList_alias);
+}
+
+bool ListCsvProcessor::isValid(const QString &filename) {
+    ListCsvProcessor p(filename);
+    return p.isValid();
+}
+
+bool ListCsvProcessor::isValid() {
+    bool status = false;
+    if (_filename!="") {
+        if (QFile::exists(_filename)) {
+            status = performCheck();
+        }
+        else {
+            status = false;
+        }
+    }
+    else {
+        status = false;
+    }
+    return status;
+}
+
+int ListCsvProcessor::readsListsFromFile(QStringList &slist_raw, QStringList &sList_alias) {
+    int status;
+    QFile file(_filename);
     if(file.open(QIODevice::ReadOnly | QIODevice::Text)) {
         QTextStream textIn(&file);
         QString lineStr;
@@ -29,29 +63,50 @@ int ListCsvProcessor::readsListsFromFile(const QString &filename, QStringList &s
             slist_raw << strList.front();
             sList_alias << strList.back();
         }
-        file.close();
-        _status = 0;
+        status = 0;
     }
     else {
-        _status = 1;
+        status = 1;
     }
-    return _status;
+    file.close();
+    return status;
 }
 
-int ListCsvProcessor::saveListsToFile(const QString &filename, const QStringList &sList_raw, const QStringList &sList_alias) {
-    QFile file(filename);
+int ListCsvProcessor::saveListsToFile(const QStringList &sList_raw, const QStringList &sList_alias) {
+    int status;
+    QFile file(_filename);
     if(file.open(QIODevice::WriteOnly | QIODevice::Text)) {
         QTextStream out(&file);
         for (int idx = 0 ; idx < sList_raw.size() ; ++idx) {
             out << sList_raw.at(idx) << "," << sList_alias.at(idx) << "\n";
         }
-        file.close();
-        _status = 0;
+        status = 0;
     }
     else {
-        _status = 1;
+        status = 1;
     }
-    return _status;
+    file.close();
+    return status;
+}
+
+bool ListCsvProcessor::performCheck() {
+    bool status = true;
+    QFile file(_filename);
+    if(file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+        QTextStream textIn(&file);
+        QString lineStr;
+        while (textIn.readLineInto(&lineStr)) {
+            if ((lineStr.count(",")+1) != 2) {
+                status = false;
+                break;
+            }
+        }
+    }
+    else {
+        status = false;
+    }
+    file.close();
+    return status;
 }
 
 }
--- AddRemoveSelectionList/Util/ListCsvProcessor.h
+++ AddRemoveSelectionList/Util/ListCsvProcessor.h
@@ -1,5 +1,5 @@
-#ifndef SIGNALLISTPROCESSOR_H
-#define SIGNALLISTPROCESSOR_H
+#ifndef ListCsvProcessor_H
+#define ListCsvProcessor_H
 
 #include <QStringList>
 
@@ -11,18 +11,39 @@ namespace Util
  * populating the selected list.
  */
 class ListCsvProcessor {
+
 public:
     ListCsvProcessor();
+    ListCsvProcessor(const QString &filename);
     ~ListCsvProcessor() { ; }
-    int static read(const QString &filename, QStringList &sList_raw, QStringList &sList_alias);    
+    // Set the file name for read or write
+    void setFile(const QString &filename) { _filename = filename; }
+    // Use the predefined file name to populate the lists.
+    int getLists(QStringList &sList_raw, QStringList &sList_alias);
+    // Use the predefiend file name to save the lists.
+    int writeLists(const QStringList &sList_raw, const QStringList &sList_alias);
+    // Validation
+    bool isValid();
+
+public: // Static
+    // Static function that read the lists from a file. status = 0 means no error, otherwise, return a error code number.
+    int static read(const QString &filename, QStringList &sList_raw, QStringList &sList_alias);
+    // Static Write the lists to  a file. status = 0 means no error, otherwise, return a error code number.
     int static write(const QString&filename, const QStringList &sList_raw, const QStringList &sList_alias);
+    // Validate a csv file
+    bool static isValid(const QString &filename);
 
 private:
-    int readsListsFromFile(const QString &filename, QStringList &slist_raw, QStringList &sList_alias);
-    int saveListsToFile(const QString &filename, const QStringList &sList_raw, const QStringList &sList_alias);    
-    int _status = 0;
+    // Internal member function for reading the file
+    int readsListsFromFile(QStringList &slist_raw, QStringList &sList_alias);
+    // Internal member function for writing lists to a file
+    int saveListsToFile(const QStringList &sList_raw, const QStringList &sList_alias);
+    // Performing format check
+    bool performCheck();
+    // Absolute filepath
+    QString _filename = "";
 };
 
 }
 
-#endif // SIGNALLISTPROCESSOR_H
+#endif // ListCsvProcessor_H
